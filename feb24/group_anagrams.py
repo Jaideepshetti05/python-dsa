ead638a04230990bbf45b0b0f23214de78c1d189
jaideep shetti <Jaideepshetti55@gmail.com>
2026-02-24 21:08:12 +0530

    commit 7

---
+++ feb24/group_anagrams.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+def group_anagrams(strs):
+    anagrams = defaultdict(list)
+
+    for word in strs:
+        key = ''.join(sorted(word))
+        anagrams[key].append(word)
+
+    return list(anagrams.values())
+
+print(group_anagrams(["eat","tea","tan","ate","nat","bat"]))
