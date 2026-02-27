40f05a9ada59bfbb497121cd55531c32edfdd6f5
jaideep shetti <Jaideepshetti55@gmail.com>
2026-02-27 21:03:19 +0530

    commit 6

---
+++ feb27/queue_using_stack.py
@@ -0,0 +1,13 @@
+class MyQueue:
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x):
+        self.s1.append(x)
+
+    def pop(self):
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
