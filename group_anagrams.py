630a815c724e0e928aa7b254f1ba1583d4230cd8
jaideep shetti <Jaideepshetti55@gmail.com>
2026-02-28 21:49:09 +0530

    commit 3

---
+++ group_anagrams.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+
+def group_anagrams(strs):
+    d = defaultdict(list)
+    for s in strs:
+        key = tuple(sorted(s))
+        d[key].append(s)
+    return list(d.values())
+
+print(group_anagrams(["eat","tea","tan","ate","nat","bat"]))
