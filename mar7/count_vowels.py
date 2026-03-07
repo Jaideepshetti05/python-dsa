983c60ab1300f74223f97233fbb282092eb029b7
jaideep shetti <Jaideepshetti55@gmail.com>
2026-03-07 23:44:09 +0530

    commit 3

---
+++ mar7/count_vowels.py
@@ -0,0 +1,11 @@
+def count_vowels(s):
+    vowels = "aeiou"
+    count = 0
+
+    for c in s.lower():
+        if c in vowels:
+            count += 1
+
+    return count
+
+print(count_vowels("hello world"))
