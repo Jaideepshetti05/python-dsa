1f3ca5fcc0c6469687392ccf8422450ae053eaf7
Jaideep <jaideepshetti55@gmail.com>
2026-03-22 17:21:45 +0530

    commit 4

---
+++ mar22/lru_cache.py
@@ -0,0 +1,37 @@
+# File: lru_cache.py
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = OrderedDict()
+        self.cap = capacity
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key, value):
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)# File: lru_cache.py
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.cache = OrderedDict()
+        self.cap = capacity
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key, value):
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)
