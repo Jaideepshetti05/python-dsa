5cbf715a6bd125bdbbdbb37b787535deb0a76a0f
Jaideep <jaideepshetti55@gmail.com>
2026-05-02 20:42:52 +0530

    commit 2

---
+++ may02/json_to_csv.py
@@ -0,0 +1,10 @@
+import json
+import csv
+
+with open('data.json') as f:
+    data = json.load(f)
+
+with open('output.csv', 'w', newline='') as f:
+    writer = csv.DictWriter(f, fieldnames=data[0].keys())
+    writer.writeheader()
+    writer.writerows(data)
