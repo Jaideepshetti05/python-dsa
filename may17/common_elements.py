609bbf886e6a98b5461224d53fedc7f0d9a0123e
Jaideep <jaideepshetti55@gmail.com>
2026-05-17 21:33:03 +0530

    commit 10

---
+++ may17/common_elements.py
@@ -0,0 +1,12 @@
+# File: common_elements.py
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+
+common = []
+
+for item in list1:
+    if item in list2:
+        common.append(item)
+
+print("Common Elements:", common)
