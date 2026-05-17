e175fd11c012caabde822a3dcb2bc722b8157a75
Jaideep <jaideepshetti55@gmail.com>
2026-05-17 21:29:04 +0530

    commit 1

---
+++ may17/prime_check.py
@@ -0,0 +1,13 @@
+# File: prime_check.py
+
+num = int(input("Enter number: "))
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime Number")
+else:
+    print("Not Prime")
