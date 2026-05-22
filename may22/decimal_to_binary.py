d2f05989af24c60fa57bdea02e2e5d719fc76678
Jaideep <jaideepshetti55@gmail.com>
2026-05-22 21:47:39 +0530

    commitall

---
+++ may22/decimal_to_binary.py
@@ -0,0 +1,5 @@
+# File: decimal_to_binary.py
+
+num = int(input("Enter decimal number: "))
+
+print("Binary:", bin(num)[2:])
---
+++ may22/largest_element.py
@@ -0,0 +1,5 @@
+# File: largest_element.py
+
+numbers = [12, 45, 67, 23, 89]
+
+print("Largest:", max(numbers))
