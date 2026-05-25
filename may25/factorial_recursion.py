cbf5dfa89693199fb4b8a60e61a9dc8a422f7209
Jaideep <jaideepshetti55@gmail.com>
2026-05-25 22:06:39 +0530

    commit 7

---
+++ may25/factorial_recursion.py
@@ -0,0 +1,9 @@
+# File: factorial_recursion.py
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+num = int(input("Enter number: "))
+print("Factorial:", factorial(num))
