32f3b4325ba64f8087d60afe9e5ebdeaf6876817
Jaideep <jaideepshetti55@gmail.com>
2026-05-27 22:01:02 +0530

    commit 10

---
+++ may27/calculator.py
@@ -0,0 +1,16 @@
+# calculator.py
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+op = input("Enter operator (+,-,*,/): ")
+
+if op == '+':
+    print(a + b)
+elif op == '-':
+    print(a - b)
+elif op == '*':
+    print(a * b)
+elif op == '/':
+    print(a / b)
+else:
+    print("Invalid Operator")
