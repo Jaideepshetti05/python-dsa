89e440167a127a0fea0f0b987c599b705a0f19d6
Jaideep <jaideepshetti55@gmail.com>
2026-06-02 21:58:36 +0530

    commit 3

---
+++ jun02/insertion_sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+if __name__ == "__main__":
+    test_arr = [12, 11, 13, 5, 6]
+    print("Original array:", test_arr)
+    insertion_sort(test_arr)
+    print("Sorted array:  ", test_arr)
