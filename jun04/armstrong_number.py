cc10e430c2a7eb7e7e8042f96262db00d48295e1
Jaideep <jaideepshetti55@gmail.com>
2026-06-04 20:31:10 +0530

    commit 1

---
+++ jun04/armstrong_number.py
@@ -0,0 +1,10 @@
+num = int(input())
+temp = num
+total = 0
+
+while temp > 0:
+    digit = temp % 10
+    total += digit ** 3
+    temp //= 10
+
+print("Armstrong" if total == num else "Not Armstrong")
