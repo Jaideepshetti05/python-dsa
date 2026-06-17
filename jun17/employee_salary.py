9946b6bf5e75fac8afba2d33c4940abf88cc9f3c
Jaideep <jaideepshetti55@gmail.com>
2026-06-17 08:22:46 +0530

    commit 4

---
+++ jun17/employee_salary.py
@@ -0,0 +1,10 @@
+class Employee:
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+
+    def display(self):
+        print(self.name,self.salary)
+
+e = Employee("John",50000)
+e.display()
