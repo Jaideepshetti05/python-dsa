c65977467a16c856e28a0f1db9f0a79ffc62c2e3
Jaideep <jaideepshetti55@gmail.com>
2026-06-21 21:16:45 +0530

    commit -1

---
+++ jun20/hi.py
@@ -0,0 +1,8 @@
+name = "jaideep"
+print(name)
+a = 20
+b = 25
+sting = "hello"
+print(a+b)
+print(b-a)
+print(sting)
---
+++ jun21/log_analyzer.py
@@ -0,0 +1,4 @@
+with open("log.txt") as f:
+    lines = f.readlines()
+
+print("Total Log Entries:", len(lines))
