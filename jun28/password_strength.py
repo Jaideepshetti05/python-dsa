4c1ce80ef3e0983afee01c944f90b6b8cb090b26
Jaideep <jaideepshetti55@gmail.com>
2026-06-28 22:26:40 +0530

    commit 1

---
+++ jun28/password_strength.py
@@ -0,0 +1,12 @@
+import re
+
+password = input("Password: ")
+
+if (len(password) >= 8 and
+    re.search(r"[A-Z]", password) and
+    re.search(r"[a-z]", password) and
+    re.search(r"\d", password) and
+    re.search(r"[@#$%^&+=!]", password)):
+    print("Strong Password")
+else:
+    print("Weak Password")
