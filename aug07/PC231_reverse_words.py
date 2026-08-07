f7b97f327a7a8d5b9734d9f0ea06095a677816ba
Jaideep <jaideepshetti55@gmail.com>
2026-08-07 15:38:03 +0530

    commit 1

---
+++ aug07/PC231_reverse_words.py
@@ -0,0 +1,7 @@
+sentence = "Python Programming Language"
+
+words = sentence.split()
+reversed_sentence = " ".join(words[::-1])
+
+print("Original:", sentence)
+print("Reversed:", reversed_sentence)
