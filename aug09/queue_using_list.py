c72aab07a8e6a198a802769a531475de2bc8985b
Jaideep <jaideepshetti55@gmail.com>
2026-08-09 21:43:09 +0530

    commit 10

---
+++ aug09/queue_using_list.py
@@ -0,0 +1,12 @@
+queue = []
+
+queue.append("Alice")
+queue.append("Bob")
+queue.append("Charlie")
+
+print("Queue:", queue)
+
+removed = queue.pop(0)
+
+print("Removed:", removed)
+print("Remaining Queue:", queue)
