63b0cd78430cff6f8d398be3cd33e00abf6c4f0f
jaideep shetti <jaideepshetti55@gmail.com>
2026-08-12 20:23:39 +0530

    commit 6

---
+++ aug12/spiral_matrix.py
@@ -0,0 +1,27 @@
+def spiral(matrix):
+    result = []
+
+    while matrix:
+        result.extend(matrix.pop(0))
+
+        if matrix and matrix[0]:
+            for row in matrix:
+                result.append(row.pop())
+
+        if matrix:
+            result.extend(matrix.pop()[::-1])
+
+        if matrix and matrix[0]:
+            for row in matrix[::-1]:
+                result.append(row.pop(0))
+
+    return result
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(spiral(matrix))
